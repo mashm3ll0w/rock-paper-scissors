1f9841287064bb79be1a0c143393c8cf08be36ed
Charles Swaleh <charlswall@live.com>
2020-01-15 15:21:19 +0300

    create the BDD for the project

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//player input should be case insensitive. ie, they could enter ROCK rock or rOCk and get no errors
+
+function computerPlay(){
+  //computer selects either rock paper scissors
+  //this function makes the computer make its choice
+}
+
+function playGame(playerSelection, computerSelection){
+  //this function plays a single round of the game and checks on who wins
+  //return the results of this function
+}
+
+function game(){
+  //this plays a 5 round game, keeps the scores and reports the winner or loser at the end of the round
+}
