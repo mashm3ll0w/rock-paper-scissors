8552ce2386682da10f17a95ba587934ab16b35c6
Charles Swaleh <charlswall@live.com>
2020-01-16 15:57:19 +0300

    one round of computer vs player in playGame function

--- js/index.js
+++ js/index.js
@@ -1,27 +1,29 @@
 //jshint esversion:6
+
 //player input should be case insensitive. ie, they could enter ROCK rock or rOCk and get no errors
+//PLAYER CHOICE
+//prompt the player to enter their choice
+let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
 
 function computerPlay() {
-	//computer selects either rock paper scissors
-	//this function makes the computer make its choice
-
 	//COMPUTER RANDOM CHOICE
 	//array to chooose from
 	let choices = ["rock", "paper", "scissors"];
 	//computer chooses from the choices array
-  let computerChoice = choices[Math.floor(Math.random() * 3)];
-  return computerChoice;
+	let computerChoice = choices[Math.floor(Math.random() * 3)];
+	return computerChoice;
 }
 
 function playGame(playerSelection, computerSelection) {
 	//this function plays a single round of the game and checks on who wins
 	//return the results of this function
+	if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
+		return "You  Win!!";
+	} else if ((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper")) {
+		return "The Computer Wins!!";
+	}
 }
 
 function game() {
 	//this plays a 5 round game, keeps the scores and reports the winner or loser at the end of the round
 }
-
-//PLAYER CHOICE
-//prompt the player to enter their choice
-let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
