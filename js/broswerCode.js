27eaa0b0e1349c41e6420b75cc4fa43fec381c16
Charles Swaleh <charlswall@live.com>
2020-03-09 20:36:36 +0300

    code for one round of the game

--- js/broswerCode.js
+++ js/broswerCode.js
@@ -1,8 +1,20 @@
 // jshint esversion:6
 
-function computerPlay(){
-  let choices = ["rock", "paper", "scissors"];
-  let compSelection = choices[Math.floor(Math.random() * 3)];
-  return compSelection;
+function computerPlay() {
+	let choices = ["rock", "paper", "scissors"];
+	let compSelection = choices[Math.floor(Math.random() * 3)];
+	return compSelection;
 }
 let ComputerChoice = computerPlay();
+
+let playerChoice = prompt("Rock Paper Scissors?").toLowerCase();
+
+function oneGame(ComputerChoice, playerChoice) {
+	if ((playerChoice === "rock" && ComputerChoice === "scissors") || (playerChoice === "paper" && ComputerChoice === "rock") || (playerChoice === "scissors" && ComputerChoice === "paper")) {
+		return "You Win!!";
+	} else if ((ComputerChoice === "rock" && playerChoice === "scissors") || (ComputerChoice === "paper" && playerChoice === "rock") || (ComputerChoice === "scissors" && playerChoice === "paper")) {
+		return "AI Triumphs!!";
+	} else {
+		return "It's a Tie!!";
+	}
+}
