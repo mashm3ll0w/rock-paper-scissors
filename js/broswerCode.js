0d30faeb6cdca8a262d8ea4618fc4bd1096f518c
Charles Swaleh <charlswall@live.com>
2020-03-11 20:13:48 +0300

    add an array of slogans to win when either player wins

--- js/broswerCode.js
+++ js/broswerCode.js
@@ -11,9 +11,13 @@ let playerChoice = prompt("Rock Paper Scissors?").toLowerCase();
 
 function oneGame(computerChoice, playerChoice) {
 	if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
-		return "You Win!!";
+		let playerSlogans = ["You Win!!", "Sweet! The machine is gLiTchInG!!", "You're da Man!!", "You crushed the AI!!", "Magnifico!!"];
+		let playerPraise = playerSlogans[Math.floor(Math.random() * 5)];
+		return playerPraise;
 	} else if ((computerChoice === "rock" && playerChoice === "scissors") || (computerChoice === "paper" && playerChoice === "rock") || (computerChoice === "scissors" && playerChoice === "paper")) {
-		return "AI Triumphs!!";
+		let winSlogans = ["AI triumphs!!", "The machines are coming!!", "Bow before me, Human!!", "Tik...Tok...Haha!!", "T minus...."];
+		let chant = winSlogans[Math.floor(Math.random() * 5)];
+		return chant;
 	} else {
 		return "It's a Tie!!";
 	}
